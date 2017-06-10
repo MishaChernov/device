540137e9a142503c348b4d39facdb517a06a14ce
MishaChernov <80amulet63@gmail.com>
2017-06-10 23:00:03 +0300

    Оформляю и оживляю попап
    Дооформил попапы, написал джс
    
    Дооформил попапы, написал джс

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var popupMap = document.querySelector('.popup-map');
+var popupForm = document.querySelector('.popup-form');
+
+var btnClosePopupMap = popupMap.querySelector('.btn-close');
+var btnClosePopupForm = popupForm.querySelector('.btn-close');
+var btnOpenPopupMap = document.querySelector('.contacts__map');
+var btnOpenPopupForm = document.querySelector('.contacts__btn');
+
+var ENTER_KEY_CODE = 27;
+
+function popupMapOpen() {
+  event.preventDefault();
+  popupMap.classList.add('popup-map--active');
+}
+function popupMapClose() {
+  popupMap.classList.remove('popup-map--active');
+}
+
+btnClosePopupMap.addEventListener('click', popupMapClose);
+btnOpenPopupMap.addEventListener('click', popupMapOpen);
+
+function popupFormOpen() {
+  event.preventDefault();
+  popupForm.classList.add('popup-form--active');
+}
+function popupFormClose() {
+  popupForm.classList.remove('popup-form--active');
+}
+
+btnClosePopupForm.addEventListener('click', popupFormClose);
+btnOpenPopupForm.addEventListener('click', popupFormOpen);
+
+window.addEventListener('keydown', function(event) {
+  if(event.keyCode === ENTER_KEY_CODE) {
+    if(popupMap.classList.contains('popup-map--active')) {
+      popupMap.classList.remove('popup-map--active');
+    }
+    if(popupForm.classList.contains('popup-form--active')) {
+      popupForm.classList.remove('popup-form--active');
+    }
+  }
+});
